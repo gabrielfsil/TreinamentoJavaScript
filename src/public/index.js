43cf51fe868f8f2b465544a36feb7c206f4a9ce9
Gabriel Fernandes <gabrielfsil264@gmail.com>
2020-05-11 10:03:07 -0300

    Construção dos modulos

---
+++ src/public/index.js
@@ -0,0 +1,31 @@
+function adicionarTarefa() {
+
+    // Pegar o valor digitado no input
+    
+
+    // Criar um Item da lista
+        
+
+    // Criar Texto
+        
+
+    // Criar Botão
+        
+        
+    // Adicionar Texto e Botão no Item da lista
+        
+
+    // Fazer referência a lista
+        
+
+    // Adicionar item na lista
+        
+
+
+}
+
+function apagarTarefa(li) {
+
+    // Remover Item da lista
+
+}
