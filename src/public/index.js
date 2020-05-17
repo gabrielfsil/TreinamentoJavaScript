c4568f83c12b0af7119753121d24ca4077a1ac78
Gabriel Fernandes <gabrielfsil264@gmail.com>
2020-05-17 14:45:21 -0300

    Adiconando mais um exercício

--- src/public/index.js
+++ src/public/index.js
@@ -1,25 +1,25 @@
 function adicionarTarefa() {
 
     // Pegar o valor digitado no input
-    
+
 
     // Criar um Item da lista
-        
+
 
     // Criar Texto
-        
+
 
     // Criar Botão
-        
-        
+
+
     // Adicionar Texto e Botão no Item da lista
-        
+
 
     // Fazer referência a lista
-        
+
 
     // Adicionar item na lista
-        
+
 
 
 }
@@ -27,5 +27,15 @@ function adicionarTarefa() {
 function apagarTarefa(li) {
 
     // Remover Item da lista
+
+}
+
+
+
+function deslizar() {
+
+    // Mover o bloco para a direita
+    
+
 
 }
